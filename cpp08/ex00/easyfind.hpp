29db7b409d143323bcf3a29c08374766e4e966b8
moadboujamaa <moadboujmaa@gmail.com>
2025-03-03 14:06:41 +0000

    cpp06 doene

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mboujama <mboujama@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 12:13:37 by mboujama          #+#    #+#             */
+/*   Updated: 2025/03/03 12:30:09 by mboujama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+
+template <typename T>
+void easyfind(T param, int nb) {
+    
+}
+
+#endif
+
